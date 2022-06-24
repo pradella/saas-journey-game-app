228a031ad2a809b7e78e05ef23562a5b5d56ef51
Maurício Pradella <pradella@gmail.com>
2022-06-23 21:54:07 -0300

    data fetch

--- src/components/GameList/GameList.tsx
+++ src/components/GameList/GameList.tsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useState } from 'react';
 import GameCover from '../GameCover/GameCover';
 import styles from './GameList.module.scss';
 
@@ -6,36 +8,29 @@ type GameListProps = {
 };
 
 function GameList({ onSelectGame }: GameListProps) {
+  // Next steps:
+  // - Carregar no onload da páginaa
+  // - Criar campo de busca de jogos
+  // - Exibir de 10 em 10 (paginação)
+  // - Ordenação
+  // - Typescript no objeto games
+
+  const [games, setGames] = useState([]);
+
+  async function fetchGameList() {
+    const response = await axios.get(
+      'https://us-central1-pradella-me.cloudfunctions.net/games/user/3243454007233303489/games'
+    );
+    console.log(response.data);
+    setGames(response.data);
+  }
+
   return (
     <div className={styles.root}>
-      <GameCover
-        url="https://image.api.playstation.com/vulcan/ap/rnd/202202/1517/UyPJCxbE3EoeLtUxjoFBnsD4.png"
-        onClick={onSelectGame}
-      />
-      <GameCover
-        url="https://image.api.playstation.com/vulcan/img/rnd/202109/2115/5jeuJiXUpb1bZc0Lp1U8N5Ka.png"
-        onClick={onSelectGame}
-      />
-      <GameCover
-        url="https://image.api.playstation.com/vulcan/ap/rnd/202112/2121/qrpfY71rsvMn6beyjgStw3cH.png"
-        onClick={onSelectGame}
-      />
-      <GameCover
-        url="https://image.api.playstation.com/vulcan/img/rnd/202201/1616/THJbARAq2YiRVUv5abCQ8GYQ.png"
-        onClick={onSelectGame}
-      />
-      <GameCover
-        url="https://image.api.playstation.com/vulcan/ap/rnd/202107/0902/fS3Hhaq06TqLrbjMVplA8MaY.png"
-        onClick={onSelectGame}
-      />
-      <GameCover
-        url="https://image.api.playstation.com/vulcan/ap/rnd/202202/2218/JRMRQKmUi9PV1ZkCf8Oh4aMd.png"
-        onClick={onSelectGame}
-      />
-      <GameCover
-        url="https://image.api.playstation.com/gs2-sec/appkgo/prod/CUSA00265_00/8/i_1c2d1812f27ec71285072e191d1822134e4f9730141c88537d87837014a17295/i/icon0.png"
-        onClick={onSelectGame}
-      />
+      <button onClick={fetchGameList}>fetch my games</button>
+      {games.map((game) => (
+        <GameCover url={game.imageUrl} onClick={onSelectGame} />
+      ))}
     </div>
   );
 }
